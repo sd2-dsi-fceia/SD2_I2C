d5a9e0b1a684880aec419873e538ac21b39da70e
Gustavo Muro <gustmuro@gmail.com>
2017-02-12 19:40:45 -0300

    agregado de template a main

--- Sources/main.c
+++ Sources/main.c
@@ -1,3 +1,36 @@
+/* Copyright 2017, DSI FCEIA UNR - Sistemas Digitales 2
+ *    DSI: http://www.dsi.fceia.unr.edu.ar/
+ * Copyright 2017, Diego Alegrechi
+ * Copyright 2017, Gustavo Muro
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 /*==================[inclusions]=============================================*/
 #include "board.h"
@@ -9,22 +42,46 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
+/*==================[macros and definitions]=================================*/
 #define EJE_X	1
 #define EJE_Y	2
 #define EJE_Z	3
 
-
-// Prototipo de funciones
-void UART_Init(void);
-void PIT_Init(void);
-
-
-
+/*==================[internal data declaration]==============================*/
 int choice = EJE_Z;
 int16_t TEST_ACC = false;
 
+/*==================[internal functions declaration]=========================*/
+void UART_Init(void);
+void PIT_Init(void);
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+void UART_Init(void)
+{
+	// Enable the UART transmitter and receiver
+
+	LPSCI_HAL_SetIntMode(UART0, kLpsciIntRxDataRegFull, true);
+	/* habilita transmisor y receptor */
+	LPSCI_HAL_EnableReceiver(UART0);
+	LPSCI_HAL_EnableTransmitter(UART0);
+}
+
+void PIT_Init(void)
+{
+	SIM_HAL_EnableClock(SIM, kSimClockGatePit0);
+	PIT_HAL_Enable(PIT);
+	PIT_HAL_SetTimerPeriodByCount(PIT, 1, 0xFFFFF);
+	PIT_HAL_SetIntCmd(PIT, 1, true);
+	PIT_HAL_SetTimerRunInDebugCmd(PIT, false);
+	PIT_HAL_StartTimer(PIT, 1);
+}
+
+/*==================[external functions definition]==========================*/
 
 int main(void)
 {
@@ -32,11 +89,8 @@ int main(void)
 
 	char Buffer[10];
 
-
     // Enable clock for PORTs, setup board clock source, config pin
     board_init();
-
-
 
     // Se inicializa el UART0
     UART_Init();
@@ -75,32 +129,7 @@ int main(void)
     	}
     	TEST_ACC = false;
    }
-
 }
-
-
-void UART_Init(void)
-{
-	// Enable the UART transmitter and receiver
-
-	LPSCI_HAL_SetIntMode(UART0, kLpsciIntRxDataRegFull, true);
-	/* habilita transmisor y receptor */
-	LPSCI_HAL_EnableReceiver(UART0);
-	LPSCI_HAL_EnableTransmitter(UART0);
-
-}
-
-
-void PIT_Init(void)
-{
-	SIM_HAL_EnableClock(SIM, kSimClockGatePit0);
-	PIT_HAL_Enable(PIT);
-	PIT_HAL_SetTimerPeriodByCount(PIT, 1, 0xFFFFF);
-	PIT_HAL_SetIntCmd(PIT, 1, true);
-	PIT_HAL_SetTimerRunInDebugCmd(PIT, false);
-	PIT_HAL_StartTimer(PIT, 1);
-}
-
 
 void UART0_IRQHandler(void)
 {
@@ -123,12 +152,26 @@ void UART0_IRQHandler(void)
 	LPSCI_HAL_ClearStatusFlag(UART0,kLpsciRxActiveEdgeDetect);
 }
 
-
 void PIT_IRQHandler(void)
 {
 	PIT_HAL_ClearIntFlag(PIT, 1);
 	TEST_ACC = true;
-
 }
+
+/*==================[end of file]============================================*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
